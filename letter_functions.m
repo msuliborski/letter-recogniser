9208eecf44abe246ce6b41cac2fcb7a1d850ee2c
Arkadiusz Zasina <34420693+217868@users.noreply.github.com>
2020-06-28 00:47:34 +0100

    gui

--- letter_functions.m
+++ letter_functions.m
@@ -52,7 +52,7 @@ classdef letter_functions
             filePath1 = strcat(filePath, '\');
             %extension = '/*.png';
             folder_pngs_path =  strcat(filePath1, '*.png');
-            images  = dir(folder_pngs_path );
+            images  = dir(folder_pngs_path )
             nfile = max(size(images));
 
             for i=1:nfile
