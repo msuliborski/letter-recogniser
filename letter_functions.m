5828721aef923a70e1391fb94afee7056fffadda
Michał Suliborski <msuliborski@gmail.com>
2020-06-27 18:16:36 +0100

    merge

--- letter_functions.m
+++ letter_functions.m
@@ -1,139 +1,227 @@
 classdef letter_functions
     methods(Static)
         function[inputs, targets] = readDataSetVowel(set_name, vowel, matrixLabel)
-        inputs = [];
-        targets = [];
-        filePath = fullfile(set_name, vowel);
-        filePath1 = strcat(filePath, '\');
-        %extension = '/*.png';
-        folder_pngs_path =  strcat(filePath1, '*.png');
-        images  = dir(folder_pngs_path );
-        nfile = max(size(images));
+            inputs = [];
+            targets = [];
+            filePath = fullfile(set_name, vowel);
+            filePath1 = strcat(filePath, '\');
+            %extension = '/*.png';
+            folder_pngs_path =  strcat(filePath1, '*.png');
+            images  = dir(folder_pngs_path );
+            nfile = max(size(images));
 
-        for i=1:nfile
-            image = imread([filePath1 images(i).name]);
-            OneDArray = reshape(image',[1 size(image,1)*size(image,2)]);
-            inputs(:,end+1) = OneDArray;
-            targets(:,end+1) = matrixLabel;
+            for i=1:nfile
+                image = imread([filePath1 images(i).name]);
+                OneDArray = reshape(image',[1 size(image,1)*size(image,2)]);
+                inputs(:,end+1) = OneDArray;
+                targets(:,end+1) = matrixLabel;
+            end
         end
-     end
-    function[inputs, targets] = readDataSet(set_name)
-        inputA = [];
-        inputE = [];
-        inputI = [];
-        inputO = [];
-        inputU = [];
+        function[inputs, targets] = readDataSet(set_name)
+            inputA = [];
+            inputE = [];
+            inputI = [];
+            inputO = [];
+            inputU = [];
 
-        targetA = [];
-        targetE = [];
-        targetI = [];
-        targetO = [];
-        targetU = [];
+            targetA = [];
+            targetE = [];
+            targetI = [];
+            targetO = [];
+            targetU = [];
 
-        [inputA, targetA] =  letter_functions.readDataSetVowel(set_name, 'A', [1 0 0 0 0]);
-        [inputE, targetE] =  letter_functions.readDataSetVowel(set_name, 'E', [0 1 0 0 0]);
-        [inputI, targetI] =  letter_functions.readDataSetVowel(set_name, 'I', [0 0 1 0 0]);
-        [inputO, targetO] =  letter_functions.readDataSetVowel(set_name, 'O', [0 0 0 1 0]);
-        [inputU, targetU] =  letter_functions.readDataSetVowel(set_name, 'U', [0 0 0 0 1]);
+            [inputA, targetA] =  letter_functions.readDataSetVowel(set_name, 'A', [1 0 0 0 0]);
+            [inputE, targetE] =  letter_functions.readDataSetVowel(set_name, 'E', [0 1 0 0 0]);
+            [inputI, targetI] =  letter_functions.readDataSetVowel(set_name, 'I', [0 0 1 0 0]);
+            [inputO, targetO] =  letter_functions.readDataSetVowel(set_name, 'O', [0 0 0 1 0]);
+            [inputU, targetU] =  letter_functions.readDataSetVowel(set_name, 'U', [0 0 0 0 1]);
 
-        inputs = [inputA inputE inputI inputO inputU];
-        targets = [targetA targetE targetI targetO targetU];
-    end
-
-    function net = createNet(netInfo)
-
-        networkConfiguration = [];
-        networkConfiguration = stringToArray(netInfo);
-        net = feedforwardnet(networkConfiguration);
-
-    end
-
-
-    function net = selectTraining(netIn, trainFunction)
-        net = netIn;
-        net.trainFcn = trainFunction;
-    end
-
-    function net = selectActivation(netIn, actvationFunction)
-        net = netIn;
-        layerCount = size(net.Layers);
-
-        for i=1:layerCount
-            net.layers{i}.transferFcn = actvationFunction;
-        end
-    end
-
-    function [net, tr] = trainFunction(netIn, inputs, targets)
-        net = netIn;
-       [net, tr] =  train(net, inputs, targets);
-    end
-
-    function accuracy = getAccuracyForVowelFolder(net, set, vowel)
-        matrixVowel = [];
-        inputs = [];
-        targets = [];
-
-        if vowel == 'A'
-            matrixVowel = [1 0 0 0 0];
-        elseif vowel == 'E'
-             matrixVowel = [0 1 0 0 0];
-        elseif vowel == 'I'
-             matrixVowel = [0 0 1 0 0];
-        elseif vowel == 'O'
-             matrixVowel = [0 0 0 1 0];
-        elseif vowel == 'U'
-             matrixVowel = [0 0 0 0 1];
+            inputs = [inputA inputE inputI inputO inputU];
+            targets = [targetA targetE targetI targetO targetU];
         end
 
-        [inputs, targets] = readDataSetVowel(set, vowel, matrixVowel);
-        out = sim(net, inputs);
+        function net = createNet(netInfo)
 
-        r = 0;
-        for i=1:size(out,2)               
-          [a b] = max(out(:,i));         
-          [c d] = max(targets(:,i));  
-          if b == d                      
-              r = r+1;
-          end
+            networkConfiguration = [];
+            networkConfiguration = letter_functions.stringToNumericArray(netInfo);
+            disp(networkConfiguration);
+            net = feedforwardnet(networkConfiguration);
+
         end
 
-        accuracy = r/size(out,2)*100;
-    end
 
-    function accuracy = getAccuracyForSet(net, set)
-        inputs = [];
-        targets = [];
-
-        [inputs, targets] = readDataSet(set);
-        out = sim(net, inputs);
-
-        r = 0;
-        for i=1:size(out,2)               
-          [a b] = max(out(:,i));          
-          [c d] = max(targets(:,i))  ;
-          if b == d                      
-              r = r+1;
-          end
+        function net = selectTraining(netIn, trainFunction)
+            net = netIn;
+            net.trainFcn = trainFunction;
         end
 
-        accuracy = r/size(out,2)*100;
-    end
-    
-    function accuracy = getAccuracyOfInput(net, inputs, targets)
-        out = sim(net, inputs);
+        function net = selectActivation(netIn, actvationFunction)
+            net = netIn;
+            layerCount = size(net.Layers);
 
-        r = 0;
-        for i=1:size(out,2)               
-          [a b] = max(out(:,i));        
-          [c d] = max(targets(:,i));  
-          if b == d                      
-              r = r+1;
-          end
+            for i=1:layerCount
+                net.layers{i}.transferFcn = actvationFunction;
+            end
         end
 
-        accuracy = r/size(out,2)*100;
-    end  
-    
-    end
+        function [net, tr] = trainFunction(netIn, inputs, targets)
+            net = netIn;
+           [net, tr] =  train(net, inputs, targets);
+        end
 
+        function accuracy = getAccuracyForVowelFolder(net, set, vowel)
+            matrixVowel = [];
+            inputs = [];
+            targets = [];
+
+            if vowel == 'A'
+                matrixVowel = [1 0 0 0 0];
+            elseif vowel == 'E'
+                 matrixVowel = [0 1 0 0 0];
+            elseif vowel == 'I'
+                 matrixVowel = [0 0 1 0 0];
+            elseif vowel == 'O'
+                 matrixVowel = [0 0 0 1 0];
+            elseif vowel == 'U'
+                 matrixVowel = [0 0 0 0 1];
+            end
+
+            [inputs, targets] = readDataSetVowel(set, vowel, matrixVowel);
+            out = sim(net, inputs);
+
+            r = 0;
+            for i=1:size(out,2)               
+              [a b] = max(out(:,i));         
+              [c d] = max(targets(:,i));  
+              if b == d                      
+                  r = r+1;
+              end
+            end
+
+            accuracy = r/size(out,2)*100;
+        end
+
+        function accuracy = getAccuracyForSet(net, set)
+            inputs = [];
+            targets = [];
+
+            [inputs, targets] = readDataSet(set);
+            out = sim(net, inputs);
+
+            r = 0;
+            for i=1:size(out,2)               
+              [a b] = max(out(:,i));          
+              [c d] = max(targets(:,i))  ;
+              if b == d                      
+                  r = r+1;
+              end
+            end
+
+            accuracy = r/size(out,2)*100;
+        end
+
+        function accuracy = getAccuracyOfInput(net, inputs, targets)
+            out = sim(net, inputs);
+
+            r = 0;
+            for i=1:size(out,2)               
+              [a b] = max(out(:,i));        
+              [c d] = max(targets(:,i));  
+              if b == d                      
+                  r = r+1;
+              end
+            end
+
+            accuracy = r/size(out,2)*100;
+        end  
+        
+        function convImgToBinary(pathToImage)
+            if ~isfile(pathToImage)
+                disp('It is not a file!');
+                return;
+            end
+
+            theFile = dir(fullfile(pathToImage));
+
+            baseFileName = theFile.name;
+            fullFileName = fullfile(pathToImage);
+            fprintf('Now reading \n');
+
+            image = imread(fullFileName);
+
+            if islogical(image)
+                return;
+            end
+
+            [rows, columns, numberOfColorChannels] = size(image);
+            if numberOfColorChannels == 3
+                greyImage = rgb2gray(image);
+            else
+                greyImage = image;
+            end
+
+            binaryImageArray = imbinarize(greyImage, 0.5);
+
+            baseFileName = strrep(fullFileName, '.jpg', '.png');
+            outputFileName = fullfile(baseFileName);
+            fprintf(1, 'Now deleting previous file \n');
+            delete(fullFileName)
+            fprintf(1, 'Now writing \n');
+            imwrite(binaryImageArray, outputFileName);
+        end
+
+        function convFolderToBinary(pathToFolder)
+            if ~isfolder(pathToFolder)
+                disp('It is not a folder!');
+                return;
+            end
+            filePatternPNG = fullfile(pathToFolder, '*.png');
+            filePatternJPG = fullfile(pathToFolder, '*.jpg');
+            theFiles = [dir(filePatternPNG); dir(filePatternJPG)];
+            numFiles = length(theFiles);
+            if numFiles == 0 
+                disp('No files in the folder!');
+                return;
+            end
+
+            for i = 1 : numFiles
+                baseFileName = theFiles(i).name;
+                fullFileName = fullfile(pathToFolder, baseFileName);
+                fprintf('Now reading %s (#%d of %d).\n', fullFileName, i, numFiles);
+
+                image = imread(fullFileName);
+
+                if islogical(image)
+                    continue;
+                end
+
+                [rows, columns, numberOfColorChannels] = size(image);
+                if numberOfColorChannels == 3
+                    greyImage = rgb2gray(image);
+                else
+                    greyImage = image;
+                end
+
+                binaryImageArray = imbinarize(greyImage, 0.5);
+
+                baseFileName = strrep(lower(baseFileName), '.jpg', '.png');
+                outputFileName = fullfile(pathToFolder, baseFileName);
+                fprintf(1, 'Now deleting previous file %s\n', outputFileName);
+                delete(fullFileName)
+                fprintf(1, 'Now writing %s\n', outputFileName);
+                imwrite(binaryImageArray, outputFileName);
+            end
+        end
+
+
+        function arr = stringToNumericArray(string)
+            arr = split(string, ' ');
+            arr = arr';
+            for i = 1 : size(arr, 2)
+                arr{1, i} = str2num(arr{1, i});
+            end
+            arr = cell2mat(arr);
+        end
+
+    end
 end
--- test_folder_1.m
+++ test_folder_1.m
@@ -3,7 +3,7 @@ inputs = [];
 targets = [];
 
 [inputs, targets] = letter_functions.readDataSet('set_1');
-net = feedforwardnet([10]);
+net = letter_functions.createNet('10 20 10')
 net = letter_functions.selectTraining(net, 'trainlm');
 net = letter_functions.selectActivation(net, 'purelin');
 [net, tr] = letter_functions.trainFunction(net, inputs, targets);
