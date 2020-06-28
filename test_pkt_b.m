89e741021f95bad2e6c07cc62a0d7c2dcd81bc3a
Arkadiusz Zasina <34420693+217868@users.noreply.github.com>
2020-06-28 01:20:04 +0100

    xxx

--- test_pkt_b.m
+++ test_pkt_b.m
@@ -2,16 +2,16 @@
 %%% Test
 clear;
 % Used: 10   5,5   10,10   10,20,30   10,20,30,40,60 
-net = letter_functions.createNet('10 20 30 40 60'); 
+net = letter_functions.createNet('10 20 30'); 
 % Used: trainlm, trainbfg, trainbr, traingda, trainr
-net = letter_functions.selectTraining(net, 'trainlm'); 
+net = letter_functions.selectTraining(net, 'trainbfg'); 
 % Used: tansig, purelin, hardlim, poslin, logsig, logsig
 net.layers{1}.transferFcn = 'tansig';
 net.layers{2}.transferFcn = 'tansig';
 net.layers{3}.transferFcn = 'tansig';
-net.layers{4}.transferFcn = 'tansig';
-net.layers{5}.transferFcn = 'tansig';
-net.layers{6}.transferFcn = 'purelin';
+%net.layers{4}.transferFcn = 'tansig';
+%net.layers{5}.transferFcn = 'tansig';
+net.layers{4}.transferFcn = 'purelin';
 
 [inputs, targets] = letter_functions.getDataset('2');
 net = letter_functions.segmentData(net, 0.7, 0.15, 0.15);
